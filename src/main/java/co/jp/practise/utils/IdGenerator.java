5a7612420cbffbe0e540831ee5af9808785d1092
wenyang.diao <wenyang.diao@51fanxing.co.jp>
2023-02-09 15:26:55 +0900

    add

--- src/main/java/co/jp/practise/utils/IdGenerator.java
+++ src/main/java/co/jp/practise/utils/IdGenerator.java
@@ -22,17 +22,21 @@ public class IdGenerator {
         Random random = new Random();
         while (count < 8) {
             int randomAscii = random.nextInt(122);
+            // 0 - 9
             if (randomAscii >= 48 && randomAscii <= 57) {
                 randomChars[count] = (char) ('0' + (randomAscii - 48));
                 count++;
+                // A - Z
             } else if (randomAscii >= 65 && randomAscii <= 90) {
                 randomChars[count] = (char) ('A' + (randomAscii - 65));
                 count++;
+                // a - z
             } else if (randomAscii >= 97 && randomAscii <= 122) {
                 randomChars[count] = (char) ('a' + (randomAscii - 97));
                 count++;
             }
         }
+        // id生成的规则： 本机名的最后一个字段-当前时间戳-8位的随机字符串
         id = String.format("%s-%d-%s", hostName, System.currentTimeMillis(), new String(randomChars));
         return id;
     }
